d0f110e8b3c609d2f8b32d34324216c9e77197cb
saileshrijal <saileshrijal1@gmail.com>
2022-07-28 20:25:58 +0545

    completed

--- src/script.js
+++ src/script.js
@@ -6,14 +6,19 @@ const thumbnail_downloader = (url) => {
     let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
     let match = url.match(regExp);
     if (match && match[1].length == 11) {
+        showLoading();
+
         let vidurl = match[1];
         console.log(vidurl)
         thumbnailHd = 'http://img.youtube.com/vi/' + vidurl + '/maxresdefault.jpg';
-        thumbnailMd = 'http://img.youtube.com/vi/' + vidurl + '/hqdefault.jpg';
+        thumbnailMd = 'http://img.youtube.com/vi/' + vidurl + '/0.jpg';
         thumbnailSmall = 'http://img.youtube.com/vi/' + vidurl + '/mqdefault.jpg';
     } else {
-        alert("The URL you have entered maybe incorrect. Please Enter a correct URL.");
-        location.reload();
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please Input URL In Input Box',
+        })
     }
     document.getElementById('hdImage').src = thumbnailHd;
     document.getElementById('mediumImage').src = thumbnailMd;
@@ -25,7 +30,36 @@ generateBtn.addEventListener('click', () => {
         thumbnail_downloader(inputUrl.value);
     }
     else {
-        alert("Enter url")
-        console.log("asdf")
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Input URL is empty or invalid',
+        })
     }
 })
+
+
+function showLoading() {
+    let timerInterval
+    Swal.fire({
+        title: 'Grabbing Thumbnail From URL!',
+        html: 'Wait <b></b> milliseconds.',
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft()
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+        }
+    })
+}
